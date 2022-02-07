efdec97bd84e54706d2112f7ff1a8474dafaf9c9
Labib <labibkamal20@gmail.com>
2022-02-06 22:33:22 -0500

    restarted project, created object containing message words, wrote funciton skeleton with errors

--- Surviving-A-DeathClaw.md
+++ Surviving-A-DeathClaw.md
@@ -1,19 +1,16 @@
-# Surviving a Deathclaw
-
-After establishing yet another settlement, The Lone Wanderer has been travelling day and night and day again to make his way back to Goodneighbour. The barren wasteland of Commonwealth has nothing that she has not bested before, and with her trusty companion Dogmeat by her side she feels ### . The scorching heat of the sun drains her stamina and she opens a cold Nuka-Cola. Another days journey lies ahead. 
-
-Dogmeat suddenly starts to bark and jump around as if he senses danger. The ground rumbles and she senses the coming danger. But before she can start to prepare, a deathclaw emerges from the ground below and she helplessly falls to the ground. The deathclaw roars loud trembling the air, and rushes towards her. With no time to waste, she reaches for any weapon she can grasp.
-
-// Companions
-Dogmeat
-Codsworth
-Hancock
-Piper
-
-//Weapons
-The deliverer
 
 
 
+Every
 
+year, day, night, week, decade, century, hour
 
+at least
+
+5, 100, 6, 69, 4000, 35619, 
+
+males, females, Europeans, North Americans, dogs, cats, mosquitoes, 
+
+eats, smooches, fights, sings, cries, goes to, dances to, thinks about
+
+the purpose of life, bananas,
--- mixed-messages.js
+++ mixed-messages.js
@@ -1,3 +1,48 @@
-function generateRandomNum(num) {
-    
+function randomNum(num) {
+    return Math.floor(Math.random * num)
 }
+
+const allWords = {
+    every: ['Every '],
+    time: ['year ', 'day ', 'night ', 'week ', 'decade ', 'century ', 'hour '],
+    atLeast: ['at least '],
+    number: [5, 100, 6, 69, 4000, 35619],
+    noun: ['males ', 'females ', 'Europeans ', 'North Americans ', 'dogs ', 'cats ', 'mosquitoes '],
+    verb: ['eats ', 'smooches ', 'fights ', 'sings ', 'cries ', 'goes to ', 'dances to ', 'thinks about '],
+    subjects: ['the Pythogorean Theorem', 'a Banana', 'reason to live', '']
+}
+
+let sentenceArray = []
+
+
+function randomMessage() {
+    for (const i in Object.keys(allWords)) {
+        switch (i) {
+            case every: sentenceArray.push(randomNum(i.length))
+            break;
+            case time: sentenceArray.push(randomNum(i.length))
+            break;
+            case atLeast: sentenceArray.push(randomNum(i.length))
+            break;
+            case number: sentenceArray.push(randomNum(i.length))
+            break;
+        }
+    }
+    console.log(sentenceArray)
+}
+
+randomMessage();
+    /*
+    let i = 
+    switch (i in allWords) {
+        case every: sentenceArray.push('Every')
+        break;
+        case time: sentenceArray.push()
+    }
+    
+
+    console.log(sentenceArray)
+}
+*/    
+
+
